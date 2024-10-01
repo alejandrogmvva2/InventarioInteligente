80a8edc53ab74d76735bd45f7a2e1fb220a98983
alejandrogmvva2 <42662368+alejandrogmvva2@users.noreply.github.com>
2024-10-01 18:15:03 +0200

    Delete src/main/java/com/inventario directory

--- src/main/java/com/inventario/Producto.kt
+++
@@ -1,9 +0,0 @@
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "productos")
-data class Producto(
-    @PrimaryKey val id: String,
-    val nombre: String,
-    val estanteriaId: String
-)
