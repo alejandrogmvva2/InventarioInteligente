5b5c716933ea6ca848ca94c9d3e825c4f97731fd
alejandrogmvva2 <42662368+alejandrogmvva2@users.noreply.github.com>
2024-10-01 18:19:31 +0200

    Create CaptureActivity.kt

---
+++ src/main/java/com/inventario/CaptureActivity.kt
@@ -0,0 +1,15 @@
+package com.inventario
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class CaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Lógica para capturar el código de barras aquí
+    }
+}
